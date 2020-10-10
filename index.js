44d2ffbe3ecb7db30939dc46f9e1dde575b5cd70
wdillard <dillardwa@gmail.com>
2020-10-10 11:49:20 -0500

    title update

---
+++ index.js
@@ -0,0 +1,17 @@
+const prompt = require('readline-sync');
+
+console.log('******         *****         *****');
+console.log('*    *        *    *        *   *');
+console.log('*    *       *     *       *   *');
+console.log('*    *      *      *      *   *');
+console.log('*    *     *       *     *   *');
+console.log('*    *    *   *    *    *   *');
+console.log('*    *   *   **    *   *   *');
+console.log('*    *  *   * *    *  *   *');
+console.log('*    * *   *  *    * *   *');
+console.log('*    **   *   *    **   *');
+console.log('*    *   *    *    *   *');
+console.log('*       *     *       *');
+console.log('*      *      *      *');
+console.log('*    *        *     *');
+console.log('*****         ******');
